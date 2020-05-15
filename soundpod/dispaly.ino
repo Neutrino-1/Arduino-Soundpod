929c6c0faadfd82ebbb98417d60877c946b99e02
Srinivasan M <srinivasanm329@gmail.com>
2020-05-15 23:09:02 +0530

    New selectable areas added
    * Removed the loop menu(too much memory consumption)
    * added selectable areas for music AudioPlayer
    * removed the battery level and headphone input ICON
    * added a jump back to side menu ICON

--- soundpod/dispaly.ino
+++ soundpod/dispaly.ino
@@ -1,4 +1,4 @@
-void about()
+void settings()
 {
    u8g2.setFont(u8g2_font_ncenB14_tr);
    u8g2.drawStr(28,u8g2.getDisplayHeight()/2-5,"Soundpod");
@@ -12,70 +12,59 @@ void about()
 
 void flashPage()
 {
-  
+  drawIcon(u8g2_font_open_iconic_play_4x_t,u8g2.getDisplayWidth()/2-12,u8g2.getDisplayHeight()-22,64); 
+  u8g2.setFont(u8g2_font_glasstown_nbp_tf);
+  u8g2.drawStr(50,u8g2.getDisplayHeight()-5,"Soundpod"); 
 }
 
-void volumeControl()
+void fileManager()
 {
-    
+  
 }
  
 void topMenu()
 {
     u8g2.setFont(u8g2_font_open_iconic_play_1x_t);
-    u8g2.drawGlyph(u8g2.getDisplayWidth()-19,9,79);
-    u8g2.drawGlyph(u8g2.getDisplayWidth()-30,9,67);
-    u8g2.setFont(u8g2_font_open_iconic_embedded_1x_t);   
-    u8g2.drawGlyph(u8g2.getDisplayWidth()-9,9,73);
+    u8g2.drawGlyph(u8g2.getDisplayWidth()-9,9,79);
 }
 
 void sideMenu()
 {
-    uint16_t menuList[4][2] = {{67,u8g2_font_open_iconic_mime_2x_t},{77,u8g2_font_open_iconic_play_2x_t},{72,u8g2_font_open_iconic_embedded_2x_t},{77,u8g2_font_open_iconic_embedded_2x_t}};
-    int Menu_1 = 0;
-    int Menu_2 = 1;
-    int Menu_3 = 2;
-    
-    if(sMenuSelection > 3 || sMenuSelection < 0)
-    {
-      Menu_1 = (abs(sMenuSelection) % 4);
-      Menu_2 = (abs(sMenuSelection+1) % 4);
-      Menu_3 = (abs(sMenuSelection+2) % 4);
-      Serial.println("Inside cyclic loop");
-    }
+    uint16_t menuList[3][2] = {{67,u8g2_font_open_iconic_mime_2x_t},{77,u8g2_font_open_iconic_play_2x_t},{72,u8g2_font_open_iconic_embedded_2x_t},};
+
     u8g2.setFontMode(0);
     
     //selection for file manager
-    if(sMenuSelection <= 1)
+    if(abs(sMenuSelection) == 1 && inSideMenuSelection)
     {
     u8g2.drawRBox(0,0,20,20,3);
     u8g2.setDrawColor(0);
     }
     //file manager
-    drawIcon(menuList[Menu_1][1],2,17,menuList[Menu_1][0]);
+    drawIcon(menuList[0][1],2,17,menuList[0][0]);
     
     u8g2.setDrawColor(1);
 
     //Audio player selection
-    if(sMenuSelection == 2)
+    if(abs(sMenuSelection) == 2 && inSideMenuSelection)
     {
     u8g2.drawRBox(0,21,20,21,3);
     u8g2.setDrawColor(0);
     }
 
     //Audio player
-    drawIcon(menuList[Menu_2][1],2,40,menuList[Menu_2][0]);
+    drawIcon(menuList[1][1],2,40,menuList[1][0]);
     
     u8g2.setDrawColor(1);
 
     //About Selection
-    if(sMenuSelection >= 3)
+    if(abs(sMenuSelection) == 3 && inSideMenuSelection)
     {
     u8g2.drawRBox(0,u8g2.getDisplayHeight()-20,20,19,3);
     u8g2.setDrawColor(0);
     }
     //About
-    drawIcon(menuList[Menu_3][1],2,62,menuList[Menu_3][0]);
+    drawIcon(menuList[2][1],2,62,menuList[2][0]);
     
     u8g2.setDrawColor(1);
     u8g2.drawLine(22,0,22,u8g2.getDisplayHeight());   
@@ -83,17 +72,61 @@ void sideMenu()
 
 void player()
 {
+  u8g2_uint_t midOriginX = u8g2.getDisplayWidth()/2;
+  u8g2_uint_t midOriginY = u8g2.getDisplayHeight()/2;
+
+  u8g2.setFont(u8g2_font_mozart_nbp_tn);
+  u8g2.drawStr(30,10,"12/455");
+    
+  u8g2.setFontMode(0);
+ 
+  if(selection == 1 && !inSideMenuSelection)
+    {
+    u8g2.drawRBox(midOriginX-7,midOriginY-5,11,9,2);
+    u8g2.setDrawColor(0);
+    }    
     u8g2.setFont( u8g2_font_open_iconic_play_1x_t);
-    u8g2.drawGlyph(u8g2.getDisplayWidth()/2-5,u8g2.getDisplayHeight()/2+4,73);
-    u8g2.drawGlyph(u8g2.getDisplayWidth()/2+25,u8g2.getDisplayHeight()/2+4,74);
+    u8g2.drawGlyph(midOriginX-5,midOriginY+4,73);
+    u8g2.setDrawColor(1);
+
+    if(selection == 2 && !inSideMenuSelection)
+    {
+    u8g2.drawRBox(midOriginX+7.5,midOriginY-8.5,16.5,15,3);
+    u8g2.setDrawColor(0);
+    }
     u8g2.setFont(u8g2_font_open_iconic_play_2x_t);
-    u8g2.drawGlyph(u8g2.getDisplayWidth()/2+7.5,u8g2.getDisplayHeight()/2+7.5,69);
-    u8g2.drawRFrame(30,u8g2.getDisplayHeight()/2+12,u8g2.getDisplayWidth()-30,5,1);
-    u8g2.drawRBox(30,u8g2.getDisplayHeight()/2+12,u8g2.getDisplayWidth()-60,5,1);
-    u8g2.setFont(u8g2_font_mozart_nbp_tn);
-    u8g2.drawStr(30,10,"12/455");
+    if(playing)
+    {
+      u8g2.drawGlyph(midOriginX+7.5,midOriginY+7.5,68);
+    }
+    else
+    {
+     u8g2.drawGlyph(midOriginX+7.5,midOriginY+7.5,69);
+    }
+    u8g2.setDrawColor(1);
+
+    if(selection == 3 && !inSideMenuSelection)
+    {
+    u8g2.drawRBox(midOriginX+25,midOriginY-5,12,9,2);
+    u8g2.setDrawColor(0);
+    }
+    u8g2.setFont(u8g2_font_open_iconic_play_1x_t);
+    u8g2.drawGlyph(midOriginX+27,midOriginY+4,74);
+    u8g2.setDrawColor(1);
+
+     if(selection == 4 && !inSideMenuSelection)
+    {
+    u8g2.drawRBox(2*midOriginX-17,2*midOriginY-10,11,8,2);
+    u8g2.setDrawColor(0);
+    }
+    u8g2.setFont(u8g2_font_open_iconic_embedded_1x_t);
+    u8g2.drawGlyph(2*midOriginX-15,2*midOriginY-1,79);
+    u8g2.setDrawColor(1);
+    
     u8g2.setFont(u8g2_font_IPAandRUSLCD_tf );
-    u8g2.drawStr(30,u8g2.getDisplayHeight()/2+30,"first_Song.mp3");
+    u8g2.drawStr(30,midOriginY+20,"first_Song.mp3");
+
+    
     sideMenu();
     topMenu();
 }
@@ -104,14 +137,8 @@ void drawIcon(const uint8_t* iconName,u8g2_uint_t  x,u8g2_uint_t  y,uint16_t  gl
     u8g2.drawGlyph(x,y,glyph);
 }
 
-void settings()
-{
-    drawIcon(u8g2_font_open_iconic_play_2x_t,30,u8g2.getDisplayHeight()/2-5,64);
-    sideMenu();
-    topMenu();
-}
-
 void fileList()
 {
-  
+  sideMenu();
+  topMenu();
 }
